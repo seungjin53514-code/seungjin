f8dd88ddc1e64386843e2f38540982c08a623846
seung <seung.jin53514@gmail.com>
2026-03-25 15:13:58 +0900

    4주차 수업

---
+++ src/foo.java
@@ -0,0 +1,23 @@
+public class foo {
+
+        // 두정수를 더하는 메소드
+        
+  public static int sum(int n, int m){
+        return n + m;
+  }
+        // main() 메소드에서 실행 시작
+    public static void main(String[] args){
+
+        int i = 20;
+        int s;
+        char a;
+
+
+        s = sum(i, 10); //som()메소드 호출
+        a = '?';
+        System.out.println(a); //문자 '?' 출력
+        System.out.println("Hello"); //문자열 Hello 출력
+        System.out.println(s); // 정수 s 출력
+}
+
+}
