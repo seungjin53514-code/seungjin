66e821cbaa33e8f99b98c533728eb9823841195b
seung <seung.jin53514@gmail.com>
2026-04-08 16:07:39 +0900

    중간

---
+++ src/Ex312.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Ex312 {
+
+	public static void main(String[] args) {
+		Scanner scanner=new Scanner(System.in);
+		int dividend;//나뉨수
+		int divisor;//나눗수
+       
+		
+		System.out.print("나뉨수를 입력하시오:");
+		dividend=scanner.nextInt(); //나뉨수 입력
+		System.out.print("나눗수를 입력하시오:");
+		divisor=scanner.nextInt(); //나눗수 입력
+		
+	
+        try {
+			System.out.println(dividend+"를"+divisor+"로 나누면 몫은 "
+				+dividend/divisor+"입니다.");
+		}
+		catch(ArithmeticException e) {
+			System.out.println("0 으로 나눌수 없습니다");
+            
+		}
+		finally {
+			scanner.close(); //scanner객체 닫기 (중요!!!!!!!!!)
+            //생략가능
+		}
+
+}
+}
