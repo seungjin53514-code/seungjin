711b7deeeb9391686817e8bcae5e8014a13ab597
seung <seung.jin53514@gmail.com>
2026-04-29 16:35:42 +0900

    9주차

--- src/Ex51.java
+++ src/Ex51.java
@@ -17,6 +17,7 @@ class Point {
 	public void set(int x, int y) {
 		this.x = x; 
         this.y = y;
+        
 	}
 	public void showPoint() { // 점의 좌표 출력
 		System.out.println("(" + x + "," + y + ")");
